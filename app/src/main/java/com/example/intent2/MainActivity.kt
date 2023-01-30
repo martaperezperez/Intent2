70950ca1277f654f33e1e677be4e5aa694cf53e5
1 <mperezperez@danielcastelao.org>
2023-01-30 09:42:35 +0100

    Funcionamiento del boto camara

--- app/src/main/java/com/example/intent2/MainActivity.kt
+++ app/src/main/java/com/example/intent2/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.intent2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.content.Intent
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //El boton para iniciar la camara
+        val callButton = findViewById<Button>(R.id.camara)
+
+        callButton.setOnClickListener {
+            //Creamos un intent específico que iniciará el teléfono
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //Llamamos a la activity teléfono
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null) {
+            val imageBitmap = data.extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
     }
 }
